446d5ec75c4043aa9ca55baef30714d4dd3152f1
mis <mish0207@yahoo.com.tw>
2014-12-09 13:17:32 +0000

    [AUTO HEROKU DEPLOY]

---
+++ fb-python-codelab/cp1/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Example for Facebook Python SDK
+Author: Colin Su <littleq0903@gmail.com>
+"""
+from bottle import Bottle, run, template
+from bottle import static_file
+from bottle import request
+from bottle import jinja2_template
+import facebook
+
+import os
+import json
+
+# setup these environment variable by adding "heroku.config"
+# - FACEBOOK_APP_ID
+# - FACEBOOK_SECRET
+FACEBOOK_APP_ID = os.environ['FACEBOOK_APP_ID']
+FACEBOOK_SECRET = os.environ['FACEBOOK_SECRET']
+
+# WSGI handler
+app = Bottle()
+
+# Checkpoint 0
+@app.route('/login')
+def login():
+    template_data = {
+        'facebook_app_id': FACEBOOK_APP_ID
+    }
+    return jinja2_template('index.html', **template_data)
+
+
+@app.route('/hello')
+def hello():
+    return "hello world"
+    
+    
+# Checkpoint 1
+@app.route('/cp1')
+def cp1():
+    cookies = request.cookies
+    fb_access_token = request.cookies['fb_access_token']
+
+    fb_graph = facebook.GraphAPI(fb_access_token)
+    profile = fb_graph.get_object("me")
+
+    return json.dumps(profile)
+    
